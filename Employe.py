a01ea321d05735367a71cf5ae7127ff6d4f4834d
GaetanC <gaetan.cren@easymile.com>
2024-03-12 09:11:15 +0100

    Change class name

---
+++ Employe.py
@@ -0,0 +1,3 @@
+from django.db import models
+
+class Employe(models.Model):
--- Role.py
+++
@@ -1,4 +0,0 @@
-from django.db import models
-
-class Role(models.Model):
-    libelle = models.CharField(max_length=50)
