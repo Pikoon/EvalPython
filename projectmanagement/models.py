7281fded0e802b4f517ee7b66c7164646ac7998c
GaetanC <gaetan.cren@easymile.com>
2024-03-14 14:43:02 +0100

    ajout parentheses

--- projectmanagement/models.py
+++ projectmanagement/models.py
@@ -3,8 +3,8 @@ from django.db import models
 
 # Create your models here.
 class Dates(models.Model):
-    debut = models.DateField
-    fin = models.DateField
+    debut = models.DateField()
+    fin = models.DateField()
 
     def __str__(self):
         return self.debut
@@ -20,7 +20,7 @@ class Employe(models.Model):
         null=True
     )
     #1 = Defaut, 2 = Responsable, 3 = Gestionnaire
-    role = models.IntegerField
+    role = models.IntegerField()
 
     def __str__(self):
         return self.name
@@ -37,14 +37,14 @@ class Tache(models.Model):
     )
     duree = models.DurationField()
     #1 = Planifiée, 2=En cours, 3 = Réalisée, 4=En pause, 5=Validée
-    statut = models.IntegerField
+    statut = models.IntegerField()
     assigned = models.ManyToManyField(
         Employe,
         through="Assignee",
         through_fields=("group", "employe")
     )
     etat_avancement = models.FloatField()
-    priorite = models.IntegerField
+    priorite = models.IntegerField()
 
     super_tache = models.ForeignKey(
         'self',
@@ -59,8 +59,8 @@ class Tache(models.Model):
 
 class Projets(models.Model):
     nom = models.CharField(max_length=255)
-    statut = models.IntegerField
-    etat_avancement = models.FloatField
+    statut = models.IntegerField()
+    etat_avancement = models.FloatField()
     date = models.ForeignKey(
         Dates,
         models.SET_NULL,
